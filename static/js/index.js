f66e0dddd528e93146ff17f494f1584c33652614
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2021-07-16 21:30:42 +0200

    No library implementation in this version

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+
+
+const div = document.createElement('div');
+const h4 = document.createElement('h4');
+const a = document.createElement('a');
+const img = document.createElement('img');
+const nav = document.createElement('nav');
