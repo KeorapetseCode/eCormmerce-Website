8425746a4901fc50b1a6a639ed6d933f5ffc1590
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2021-07-17 14:31:04 +0200

    react setup and intro

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello coder do you wanna create an eCommerce Website</h1>
+		
+	
+	</div>
+  );
+}
+
+export default App;
--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const div = document.createElement('div');
-const h4 = document.createElement('h4');
-const a = document.createElement('a');
-const img = document.createElement('img');
-const nav = document.createElement('nav');
