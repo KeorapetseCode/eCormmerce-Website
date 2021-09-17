f31ea7fbd26fcef2547fa8110eddb0fb9c2cb638
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2021-09-17 17:14:39 +0200

    first attemp to buidling the upload feature

---
+++ client/src/ImageUpload.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+
+function ImageUpload(){
+
+	var [imgfile, setFile] = useState('');
+	var [filestatus, setFileStatus] = useState(false);
+
+	const changeHandler = (event) => {
+		setFile(event.target.files[0]);
+		setFileStatus(true);
+	};
+
+	const handleSubmission = () => {
+
+		const formData = new FormData();
+		formData.append('File', imgfile);
+
+		const sendImg = fetch('/api/imageFile',{method: 'POST',body: formData});
+		const response = sendImg.json();
+
+
+	};
+
+	return (
+		<div className='image_upload'>
+			<h2>File Upload</h2>
+			<div className='fileInputField'>
+				<input type="file" name="image_upload" onChange={changeHandler} />
+				
+				{filestatus ? (
+				<div>
+					<p>Filename: {imgfile.name}</p>
+					<p>Filetype: {imgfile.type}</p>
+					<p>Size in bytes: {imgfile.size}</p>
+					{/*<p>
+						lastModifiedDate:{' '}
+						{selectedFile.lastModifiedDate.toLocaleDateString()}
+					</p>*/}
+				</div>
+			) : (
+				<p>Select a file to show details</p>
+			)}
+				<button onClick={handleSubmission}>Submit</button>
+			</div>
+		</div>
+	)
+}
+
+export default ImageUpload
