7ccab985eacd9c03a8ccbd83f33b51d5eead3501
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2021-10-21 15:29:56 +0200

    able to get an array of image sources, combined with the main item image as one array

--- client/src/FullScreenView.js
+++ client/src/FullScreenView.js
@@ -1,33 +1,72 @@
 import './styles/FullScreenView.css';
-import React from 'react';
-//import React, { useEffect, useState } from "react";
+//import React from 'react';
+import React, { useEffect, useState } from "react";
 //import { SlideShowData } from './SlideShowData.js'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft, FaTimes } from 'react-icons/fa';
+import Close from '@material-ui/icons/Close';
 
-//import Close from '@material-ui/icons/Close';
 
-function FullScreenView (props) {
+function FullScreenView ({ selectedImg, setSelectedImg }) {
 
-	/*additionalImgs[0]*/
-	/*<img src={props.selectedImg} alt='fullViewItems'></img>
+	const [additionalImgs, setadditionalImgs] = useState([]);
+	const [loadAdditionalImgs, setLoadAdditionalImgs] = useState(false);
+
+	const [current, setCurrent] = useState(0);
+
+	useEffect(() => {
+		// Had to make this request a POST because GET does not sent a request with body data
+		let fetchSupportingImgs = async (imgToView) => {
+				
+			let options = {
+				method : 'POST',
+				headers : {'Content-Type' : "application/json"},
+				body : JSON.stringify({'image' : imgToView})
+			};
 		
-	*/
-	/* if statement outside return to check if supporting images are available or not
-		If not display only the props.selected image.
-		if there are, images in additionalImgs then your return() will be different.(kind of like home.js)
-		**hint**
-		if additionalImgs is available then make sure to add props.selectedImg as an object to additionalImgs.
-		this will help because now additionalImgs will have all the images you need as one array/obj
+			const resp = await fetch('/api/getSupporting', options);
+			const imgs = await resp.json();
 
-		<FaArrowAltCircleLeft className='left__arrow' onClick={() => prevPic} />
-		<FaArrowAltCircleRight className='right__arrow' onClick={() => nextPic} />
-		<img src={props.selectedImg} className='slide__image' alt='fullViewItems'></img>
-	*/
-	const closeFullView = () => props.setSelectedImg(null);
-	return (
-		<div className='slider' onClick={closeFullView}>
-			<img src={props.selectedImg} className='slide__image' alt='fullViewItems'></img>
+			//Adding main item image with supportingImages and turning the whole thin into an array.
+			let temp = JSON.parse(imgs[0].SupportingImages);
+			temp = Object.values(temp);
+			temp.unshift(selectedImg);
+
+			setadditionalImgs(temp);
+			setLoadAdditionalImgs(true);
 			
-		</div>
+		}
+		fetchSupportingImgs(selectedImg);
+	}, [selectedImg]);
+	
+	const nextSlide = () => {
+		let len = additionalImgs.length;
+
+		setCurrent(current === len - 1 ? 0 : current + 1);
+		console.log("Crr " + current);
+	}
+	const prevSlide = () => {
+		
+		let len = additionalImgs.length;
+		
+		if (current === 0)
+			setCurrent(len - 1);
+		else
+			setCurrent(current - 1);
+		console.log("Crr " + current);
+		
+	}
+
+	const closeFullView = () => setSelectedImg(null);
+	
+	return (
+		
+		<section className='slider'>
+			<FaTimes className='close__icon' onClick={closeFullView}/>
+			<FaArrowAltCircleLeft className='left__arrow' onClick={prevSlide} />
+			<FaArrowAltCircleRight className='right__arrow' onClick={nextSlide}/>
+			<img src={selectedImg} className='slide__image' alt='fullViewItems'></img>
+			
+		</section>
 	)
 
 }
